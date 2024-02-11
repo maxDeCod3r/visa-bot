d9e946f0545a9327348bbb0159381a5e1ad83496
Max Windows <me@maxhunt.design>
2024-02-11 14:32:15 +0000

    bugfix

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ class VisaBot:
         driver.get('https://visa.vfsglobal.com/gbr/en/ita/book-an-appointment')
         info("Sleeping")
         time.sleep(7.112)
-        button_target = (383, 492)
+        button_target = (359, 522)
         self.random_move_to((10, 10), button_target)
         time.sleep(0.4975)
         pyautogui.mouseDown()
@@ -112,7 +112,7 @@ class VisaBot:
 
         info("switching to new tab")
         driver.switch_to.window(driver.window_handles[-1])
-        email_field = (708, 366)
+        email_field = (371, 378)
         # wait_for_verify()
         self.random_move_to(button_target, email_field)
         time.sleep(0.23)
@@ -128,39 +128,39 @@ class VisaBot:
         self.type_with_random_intervals(self.secrets.password)
         time.sleep(0.142)
         pyautogui.press('enter')
-        time.sleep(9.365)
+        time.sleep(7.365)
 
-        new_application_button = (1119, 244)
+        new_application_button = (221, 223)
         self.random_move_to(email_field, new_application_button)
         time.sleep(0.532)
         pyautogui.mouseDown()
         time.sleep(0.4524)
         pyautogui.mouseUp()
-        time.sleep(4.245)
+        time.sleep(2.745)
 
-        field_1 = (703, 517)
-        field_1_1 = (648, 564)
+        field_1 = (334, 547)
+        field_1_1 = (378, 597)
         self.random_move_to(new_application_button, field_1)
         time.sleep(0.532)
         self.human_click()
         self.random_move_to(field_1, field_1_1)
         time.sleep(0.532)
         self.human_click()
-        time.sleep(4.532)
+        time.sleep(1.532)
 
-        field_2 = (736, 643)
+        field_2 = (425, 682)
         self.random_move_to(field_1_1, field_2)
         time.sleep(0.532)
         self.human_click()
         self.human_click()
-        time.sleep(4.532)
+        time.sleep(1.1575)
 
-        field_3 = (530, 763)
+        field_3 = (463, 802)
         self.random_move_to(field_2, field_3)
         time.sleep(0.532)
         self.human_click()
         self.human_click()
-        time.sleep(2)
+        time.sleep(1)
 
         try:
             response_element = driver.find_element(By.CLASS_NAME, "alert-info")
@@ -211,8 +211,10 @@ class Main:
         # chrome_options.add_argument("--incognito")
         # free proxy server URL
         # proxy_server_url = "173.212.227.160:26854"
+        chrome_options.add_argument("--window-position=10,11")
         # options.add_argument(f'--proxy-server={proxy_server_url}')
         driver = uc.Chrome(headless=False, use_subprocess=False, options=chrome_options)
+        driver.set_window_size(1001, 1002)
         # clear_browsing_data(driver)
         agent = VisaBot(driver, Secrets())
         result = agent.run(driver)
@@ -222,11 +224,12 @@ class Main:
         formatted_date_time = now.strftime("%d %H:%M:%S")
         labelled_result = f"({formatted_date_time}): {result} | "
         self.results.append(f"{labelled_result}")
-        info("Cycle done, sleeping for 10 seconds")
-        time.sleep(10)
+        info("Cycle done, sleeping for 5 seconds")
+        time.sleep(5)
         driver.quit()
 
     def run_loop(self):
+        self.last_upd_time = time.time()
         while self.do_run:
             try:
                 self.run_cycle()
@@ -235,13 +238,13 @@ class Main:
                 formatted_date_time = now.strftime("%d %H:%M:%S")
                 labelled_result = f"({formatted_date_time}): ERROR: {e} | "
                 self.results.append(labelled_result)
-            if len(self.results) > (3*60/10):  # if over 6 hours has passed since last results notification
-                info("6 Hour cycle complete, sending results")
+            if time.time() - self.last_upd_time > (3*60*60):  # if over 6 hours has passed since last results notification
+                info("3 Hour cycle complete, sending results")
                 results_str = ', '.join(self.results)
                 self.telegram.send_logs(results_str)
                 self.results = []
             info("Cycle completed, sleeping for 5 minutes")
-            time.sleep(7*60)
+            time.sleep(5*60)
 
 
 
